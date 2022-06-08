c081c3872d646638c3761d355da6bbea0f69c50b
sarthak <sarthakbatham26@gmail.com>
2022-06-08 21:46:30 +0530

    basic placeholder for routes

---
+++ src/routes/users.js
@@ -0,0 +1,14 @@
+import express from "express"
+let router = express.Router()
+
+router.get('/:userName/:urlCode', (req, res, next) => {
+    const userName = req.params.userName
+    const urlCode = req.params.urlCode
+    res.send("TBD");
+}
+
+)
+
+router.post('/', (req, res) => {
+    res.send("TBD");
+})
