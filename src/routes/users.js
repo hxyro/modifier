c3b3801ddeb8de2125f7a74932902c1ae71c223c
sarthak <sarthakbatham26@gmail.com>
2022-06-09 00:24:12 +0530

    add validation checks for user data sent by POST

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,6 @@
 import express from "express"
+import {body, validationResult} from 'express-validator'
+
 let router = express.Router()
 
 router.get('/:userName/:urlCode', (req, res, next) => {
@@ -9,6 +11,38 @@ router.get('/:userName/:urlCode', (req, res, next) => {
 
 )
 
-router.post('/', (req, res) => {
-    res.send("TBD");
+router.post(
+    '/',
+    body('urlCode')
+        .trim().
+        isAlphanumeric("-")
+        .withMessage('UrlCode contains invalid characters')
+        .escape(),
+    body('url')
+        .trim()
+        .isURL()
+        .withMessage('Invalid website url')
+        .escape(),
+    body('title')
+        .trim()
+        .isLength({min: 1, max: 30})
+        .withMessage('1 <= tilte <= 30')
+        .escape(),
+    body('image_url')
+        .trim()
+        .isURL()
+        .withMessage('Invalid image url')
+        .escape(),
+    body('description')
+        .trim()
+        .isLength({min: 0, max: 100})
+        .withMessage("description too long")
+        .escape(),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        res.send('TBD')
 })
