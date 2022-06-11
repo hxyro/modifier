6a6eb3859e3499bd54ac66b591c94b5194e635a8
hxyro <hxyro0@gmail.com>
2022-06-11 12:45:26 +0530

    validation

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,8 @@
 import { Router } from 'express'
 import { model } from '../models'
-import { controller } from '../controllers/controller'
+import { controller, validator } from '../controllers'
 
-const user_validate = (req, res, next) => {
-  if (req.body.user_name) {
-    next()
-  } else {
-    res.json({ error: 'user_name is required property' }).end()
-  }
-}
-
-export const router = Router()
+const router = Router()
 //TODO: add validator
 
 // Homepage
@@ -19,7 +11,13 @@ router.get('/', controller.index)
 
 // Create New User
 // -> curl -X POST http://localhost:3000/alter -d 'user_name=hxyro'
-router.post('/alter', user_validate, controller.createUser(model))
+router.post(
+  '/alter',
+  validator.required(['user_name']),
+  validator.validName(['user_name']),
+  validator.createUser(model),
+  controller.createUser(model)
+)
 
 // Delete User
 // -> curl -X DELETE http://localhost:3000/alter/hxyro
@@ -31,7 +29,15 @@ router.get('/:user_name', controller.getUser(model))
 
 // Create Modifier
 // -> curl -X POST http://localhost:3000/hxyro/webpai -d 'redirect_url=https://webpai.vercel.app/&asset_url=https://webpai.vercel.app/webpai-new.png&title=webpai&description=nut'
-router.post('/:user_name/:modifier_name', controller.createModifier(model))
+router.post(
+  '/:user_name/:modifier_name',
+  validator.required(['redirect_url', 'asset_url', 'title', 'description']),
+  validator.validUrl(['redirect_url', 'asset_url']),
+  validator.maxLength('title', 30),
+  validator.maxLength('description', 300),
+  validator.createModifier(model),
+  controller.createModifier(model)
+)
 
 // Delete Modifier
 // -> curl -X DELETE http://localhost:3000/hxyro/webpai
@@ -40,3 +46,5 @@ router.delete('/:user_name/:modifier_name', controller.deleteModifier(model))
 // Redirect
 // -> curl -X GET http://localhost:3000/hxyro/webpai
 router.get('/:user_name/:modifier_name', controller.redirect(model))
+
+export default router
