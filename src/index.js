58dd8facd742f0e80888ee792d7586da0aab588b
hxyro <hxyro0@gmail.com>
2022-06-21 19:20:32 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express, { urlencoded } from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import { router } from './routes'
 import { db } from './utils'
@@ -7,7 +8,10 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 const MONGO_URI = process.env.MONGO_URI
 
+const corsOptions = { origin: '*' }
+
 const app = express()
+app.use(cors(corsOptions))
 app.disable('x-powered-by')
 app.use(urlencoded({ extended: true }))
 app.use('/', router)
